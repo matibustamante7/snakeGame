4b3afac638ef08f77e50b91363f6f0300d20b2c0
matibustamante7 <matias.dev7@gmail.com>
2023-11-29 11:04:26 -0300

    empezando el proyecto

---
+++ script.js
@@ -0,0 +1,6 @@
+// TOMAR LOS ELEMENTOS HTML QUE NECESITAMOS UTILIZAR
+
+const board = document.getElementById('board')
+const scoreBoard = document.getElementById('scoreBoard')
+const startButton = document.getElementById('startButton')
+const gameOver = document.getElementById('gameOver')
